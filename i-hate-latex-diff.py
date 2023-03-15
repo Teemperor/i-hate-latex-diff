70d24824e990ba965ab2da32c380d23d11ae77fc
Raphael Isemann <teemperor@gmail.com>
2023-03-15 23:05:37 +0100

    Token-wise splitting

--- i-hate-latex-diff.py
+++ i-hate-latex-diff.py
@@ -26,34 +26,74 @@ def overwrite_contents(filename, contents):
 added_change_cmd = "\\addedChange{"
 removed_change_cmd = "\\removedChange{"
 cmd_end = "}"
-ignore_token = "IGNORETHISLINE"
+
+def surround_with_cmd(cmd, content):
+    if content.endswith("\n"):
+        return cmd + content[:-1] + cmd_end + "\n"
+    return cmd + content + cmd_end
+
+def make_tokens(lines):
+    """
+    Creates the latex tokens feed to the diff tool.
+    """
+
+    result = []
+    for line in lines:
+        # Split by space but preserve the space. This way we can concatenate
+        # at the end and recreate the original input.
+        tokens = line.split(" ")
+        for token in tokens[:-1]:
+            result.append(token + " ")
+        if tokens:
+            result.append(tokens[-1])
+    return result
 
 def make_latex_diff(old, new):
-    result = []
-    diff = difflib.unified_diff(old, new, n=10000, fromfile=ignore_token,
-                                tofile=ignore_token)
+    result = ""
 
-    for line in diff:
-        code = line[0]
-        content = line[1:-1]
+    # Turn the lines into a set of tokens.    
+    old_tokens = make_tokens(old)
+    new_tokens = make_tokens(new)
+
+    # Ignore the file name markers by giving the file names this weird name.
+    # We can filter for this later.
+    ignore_token = "IGNORE_THIS_LINE_BECAUSE_ITS_A_FILE_MARKER"
+
+    # Create a diff that contains the whole file in the context.
+    diff = difflib.unified_diff(old_tokens, new_tokens, n=len(old) + len(new),
+                                fromfile=ignore_token,
+                                tofile=ignore_token, lineterm="")
+    
+
+    for token in diff:
+        # The first character is a diff marker such as '+', '-' or ' '.
+        code = token[0]
+        # The raw original input.
+        content = token[1:]
+        # What should be added to the result for this token.
         to_add = ""
 
-        if ignore_token in line:
+        if ignore_token in token:
+            # This is a file indicator, don't use it as it's fake files.
             continue
         elif code == "@":
+            # This is an annotation, ignore it.
             continue
         elif code == "-":
-            to_add = removed_change_cmd + content + cmd_end
+            # This was removed, so annotate the latex code.
+            to_add = surround_with_cmd(removed_change_cmd, content)
         elif code == "+":
-            to_add = added_change_cmd + content + cmd_end
+            # This was added, so annotate the latex code.
+            to_add = surround_with_cmd(added_change_cmd, content)
         elif code == " ":
+            # Unchanged, just copy it over.
             to_add = content
         else:
             print("Unknown difflib code: " + code)
             sys.exit(1)
-        result.append(to_add)
+        result += to_add
 
-    return "\n".join(result) + "\n"
+    return result
 
 
 if __name__ == "__main__":
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import subprocess as sp
 import tempfile
@@ -11,25 +12,36 @@ tests = []
 script_dir = os.path.dirname(os.path.realpath(__file__))
 diff_path = script_dir + "/../i-hate-latex-diff.py"
 
+
+parser = argparse.ArgumentParser(
+    prog="test",
+    description="Runs the tests"
+)
+parser.add_argument("--fixup", action="store_true")
+args = parser.parse_args()
+
 if len(tests) == 0:
     for test in os.listdir(script_dir):
         if os.path.exists(path.join(script_dir, test, "expected")):
             tests.append(path.join(script_dir, test))
 
+
 def run_test(directory):
     with tempfile.TemporaryDirectory() as tmpdir:
-        sp.check_call([diff_path,
-                       path.join(directory, "a"),
-                       path.join(directory, "b"),
-                       tmpdir])
-        
+        sp.check_call(
+            [diff_path, path.join(directory, "a"), path.join(directory, "b"), tmpdir]
+        )
+
         if len(os.listdir(tmpdir)) == 0:
             return "Output folder was empty"
 
-        diff_result = sp.run(["diff", "-r", "-U1", tmpdir, directory + "/expected"], capture_output=True)
+        diff_result = sp.run(
+            ["diff", "-r", "-U1", tmpdir, directory + "/expected"], capture_output=True
+        )
         if diff_result.returncode == 0:
             return None
         return "Wrong output:\n" + diff_result.stdout.decode("utf-8")
+
 
 for test in tests:
     sys.stdout.write("Running test " + test + ": ")
